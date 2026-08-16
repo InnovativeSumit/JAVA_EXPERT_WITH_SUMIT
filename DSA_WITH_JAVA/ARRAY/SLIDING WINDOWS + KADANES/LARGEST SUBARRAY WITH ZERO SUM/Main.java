bc69a9919ca1abb21ac4f17d92c07cc1c0445412
InnovativeSumit <pal2024santosh@gmail.com>
2026-08-17 01:29:31 +0530

    DAY37

--- DSA_WITH_JAVA/ARRAY/SLIDING WINDOWS + KADANES/LARGEST SUBARRAY WITH ZERO SUM/Main.java
+++ DSA_WITH_JAVA/ARRAY/SLIDING WINDOWS + KADANES/LARGEST SUBARRAY WITH ZERO SUM/Main.java
@@ -1,5 +1,41 @@
-package DSA_WITH_JAVA.ARRAY.SLIDING WINDOWS + KADANES.LARGEST SUBARRAY WITH ZERO SUM;
+// LARGEST SUBARRAY WITH ZERO SUM
+// TC => O(NLOGN)
+// SC => O(N)
+
+import java.util.*;
 
 public class Main {
-  
+    public static int largestZeroSumSubarray(int[] arr) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        int sum = 0;
+        int maxLength = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+            // If prefix sum itself is zero
+            if (sum == 0) {
+                maxLength = i + 1;
+            }
+
+            // If prefix sum appeared before
+            if (map.containsKey(sum)) {
+                int length = i - map.get(sum);
+                maxLength = Math.max(maxLength, length);
+
+            } else {
+                // Store first occurrence only
+                map.put(sum, i);
+            }
+        }
+
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {15, -2, 2, -8, 1, 7, 10, 23};
+        int result = largestZeroSumSubarray(arr);
+        System.out.println(result);
+    }
 }
